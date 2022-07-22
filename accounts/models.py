f5f0a79bc80183a8f18f4d7e9eaf40b87a8b23e6
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-22 15:41:26 +0330

    Custo User Model editted

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField()
+    age = models.PositiveIntegerField(null=True, blank=True)
 
 
 
