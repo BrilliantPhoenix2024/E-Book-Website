88b9e4094ed97f6f1a2ee16dde227e693f641c7a
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-23 16:16:05 +0330

    some TEST for pages APP created

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,9 @@ from django.urls import reverse_lazy
 
 from .forms import CustomUserCreationForm
 
+
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+
