77ba635fe26984d6e1f7ec23738fa0e7a6d9b0e5
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-25 17:48:39 +0330

    create VIEW for BookUpdateView

--- books/views.py
+++ books/views.py
@@ -19,3 +19,9 @@ class BookCreateView(generic.CreateView):
     fields = ['title', 'author', 'content', 'price']
     template_name = 'books/book_create.html'
 
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'author', 'content']
+    template_name = 'books/book_update.html'
+
