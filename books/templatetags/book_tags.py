01478dad4632aeee6a14cb6d736628fab4057b94
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-26 19:44:50 +0330

    create CUSTOM Templatetags DIRECTORY to books app

---
+++ books/templatetags/book_tags.py
@@ -0,0 +1,16 @@
+# from django import template
+#
+# register = template.Library()
+#
+#
+# # lowercase
+# @register.filter
+# def to_lowercase(value):
+#     return value.lower()
+
+
+# lowercase
+# @register.filter
+# def to_lowercase(value, arg):
+#     return f'{arg} : {value.lower()}'
+
