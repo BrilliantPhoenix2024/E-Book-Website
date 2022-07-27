2d5a990e82e8dd32fb69f44c9ab892a011760683
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-27 21:02:36 +0330

    EDIT URLS.PY books app

--- books/urls.py
+++ books/urls.py
@@ -4,8 +4,8 @@ from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView,
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
-    path('<int:pk>', BookDetailView.as_view(), name='book_detail'),
-    path('/new/', BookCreateView.as_view(), name='book_create'),
+    path('<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('new/', BookCreateView.as_view(), name='book_create'),
     path('<int:pk>/edit/', BookUpdateView.as_view(), name='book_update'),
     path('<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
 ]
