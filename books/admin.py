cff7c8b7dd300c8d0cc598de5a7c973229c57c34
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-29 21:48:24 +0330

    EDIT books/admin.py

--- books/admin.py
+++ books/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Book, Comment
 
 
+# @admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'text', 'book', 'datetime_created']
+
+
 admin.site.register(Book)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
