2281e48053e91a38a4ff2e4591c0f760ab1d4eab
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-07-30 17:44:48 +0330

    ENABLE comment form in book_detail template with adding forms.py and edit views.py

--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, render
 
 from .models import Book
+from .forms import CommentForm
 
 
 class BookListView(generic.ListView):
@@ -18,7 +19,20 @@ class BookListView(generic.ListView):
 def book_detail_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book_comments = book.comments.all()
-    return render(request, 'books/book_detail.html', {'book': book, 'comments': book_comments})
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'books/book_detail.html',
+                  {'book': book, 'comments': book_comments,
+                   'comment_form':comment_form})
 
 
 class BookCreateView(generic.CreateView):
